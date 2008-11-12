d71d24f6bb567d1c66c7650ecf4eb8d6cdfa8919
livibetter <livibetter@localhost>
2008-11-12 09:37:48 +0000

    Add README and remove link after copying statement in each source files

--- Blogger/brps/src/brps/post.py
+++ Blogger/brps/src/brps/post.py
@@ -14,9 +14,6 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Author : Yu-Jie Lin
-# Website: http://friendfeed.com/livibetter
 
 
 import logging
