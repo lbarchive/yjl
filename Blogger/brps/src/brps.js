baa8fd0a66d988c3715a0f42e0dd3c0190475d9a
livibetter <livibetter@localhost>
2009-01-27 04:58:57 +0000

    BRPS: Made Google ajax libraries api loads latest jQuery 1.3 series

--- Blogger/brps/src/brps.js
+++ Blogger/brps/src/brps.js
@@ -16,7 +16,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-google.load('jquery', '1.3.0');
+google.load('jquery', '1.3');
 google.setOnLoadCallback(function() {
   BRPS_get();
   });
