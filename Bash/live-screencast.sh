3d7165ca892c9dff672eadc6edf29a5995610d1f
livibetter <livibetter@localhost>
2009-02-01 11:20:58 +0000

    Added screencast helper

---
+++ Bash/live-screencast.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+FPS=4
+VIDEO_DEVICE=/dev/video0
+GRAB_OFFSET=+0,25
+
+
+show_menu() {
+  echo "Live Screencast Helper
+1) Start to capture
+2) Blur screen
+3) Desaturate screen
+
+L) Load Another Video Loopback Device
+U) Unload Another Video Loopback Device
+
+Q) Quit
+"
+  }
+
+while [[ 1 ]]; do
+  show_menu
+  read -p "Choose? " choice
+  case $choice in
+    1)
+      ffmpeg -f x11grab -vc rawvideo -s 1280x960 -r $FPS -an -i :0.0$GRAB_OFFSET -f rawvideo -pix_fmt rgb24 - | mencoder - -demuxer rawvideo -rawvideo fps=$FPS:w=1280:h=960:format=rgb24 -nosound -ovc raw -vf scale=320:240,format=bgr24 -of rawvideo -ofps $FPS -o $VIDEO_DEVICE
+      ;;
+    2)
+      ffmpeg -f x11grab -vc rawvideo -s 1280x960 -r $FPS -an -i :0.0$GRAB_OFFSET -f rawvideo -pix_fmt rgb24 - | mencoder - -demuxer rawvideo -rawvideo fps=$FPS:w=1280:h=960:format=rgb24 -nosound -ovc raw -vf scale=320:240,format=bgr24 -ssf lgb=10:cgb=10 -of rawvideo -ofps $FPS -o $VIDEO_DEVICE
+      ;;
+    3)
+      ffmpeg -f x11grab -vc rawvideo -s 1280x960 -r $FPS -an -i :0.0$GRAB_OFFSET -f rawvideo -pix_fmt rgb24 - | mencoder - -demuxer rawvideo -rawvideo fps=$FPS:w=1280:h=960:format=rgb24 -nosound -ovc raw -vf hue=0:0,scale=320:240,format=bgr24 -of rawvideo -ofps $FPS -o $VIDEO_DEVICE
+      ;;
+    l|L)
+      lsmod | grep avld &> /dev/null
+      if [[ $? == 0 ]]; then
+        echo "AVLD has been loaded, please unload it first."
+        continue
+      fi
+      sudo modprobe avld width=320 height=240 fps=$FPS palette=0
+      sleep 1
+      sudo chmod 666 $VIDEO_DEVICE
+      ;;
+    u|U)
+      lsmod | grep avld &> /dev/null
+      if [[ $? == 0 ]]; then
+        sudo rmmod avld
+      fi
+      ;;
+    q|Q|x|X)
+      echo "Bye!"
+      break
+      ;;
+  esac
+
+#Check
+#mplayer tv:// -tv "driver=v4l:device=/dev/video0:noaudio:outfmt=bgr24" -vo x11 -fps 5
+
+done
+
+# vim:et:ts=2:sts=2:sw=2:ai
