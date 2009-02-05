dd7bc086146898a143dfb941e4ad234694e5ea8b
livibetter <livibetter@localhost>
2009-02-05 15:27:59 +0000

    Use variables of width and height

--- Bash/live-screencast.sh
+++ Bash/live-screencast.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-FPS=4
+FPS=5
 VIDEO_DEVICE=/dev/video0
 GRAB_OFFSET=+0,25
-
+IN_WIDTH=960
+IN_HEIGHT=720
 
 show_menu() {
   echo "Live Screencast Helper
@@ -23,13 +24,13 @@ while [[ 1 ]]; do
   read -p "Choose? " choice
   case $choice in
     1)
-      ffmpeg -f x11grab -vc rawvideo -s 1280x960 -r $FPS -an -i :0.0$GRAB_OFFSET -f rawvideo -pix_fmt rgb24 - | mencoder - -demuxer rawvideo -rawvideo fps=$FPS:w=1280:h=960:format=rgb24 -nosound -ovc raw -vf scale=320:240,format=bgr24 -of rawvideo -ofps $FPS -o $VIDEO_DEVICE
+      ffmpeg -f x11grab -vc rawvideo -s ${IN_WIDTH}x${IN_HEIGHT} -r $FPS -an -i :0.0$GRAB_OFFSET -f rawvideo -pix_fmt rgb24 - | mencoder - -demuxer rawvideo -rawvideo fps=$FPS:w=$IN_WIDTH:h=$IN_HEIGHT:format=rgb24 -nosound -ovc raw -vf scale=320:240,format=bgr24 -of rawvideo -ofps $FPS -o $VIDEO_DEVICE
       ;;
     2)
-      ffmpeg -f x11grab -vc rawvideo -s 1280x960 -r $FPS -an -i :0.0$GRAB_OFFSET -f rawvideo -pix_fmt rgb24 - | mencoder - -demuxer rawvideo -rawvideo fps=$FPS:w=1280:h=960:format=rgb24 -nosound -ovc raw -vf scale=320:240,format=bgr24 -ssf lgb=10:cgb=10 -of rawvideo -ofps $FPS -o $VIDEO_DEVICE
+      ffmpeg -f x11grab -vc rawvideo -s ${IN_WIDTH}x${IN_HEIGHT} -r $FPS -an -i :0.0$GRAB_OFFSET -f rawvideo -pix_fmt rgb24 - | mencoder - -demuxer rawvideo -rawvideo fps=$FPS:w=$IN_WIDTH:h=$IN_HEIGHT:format=rgb24 -nosound -ovc raw -vf scale=320:240,format=bgr24 -ssf lgb=10:cgb=10 -of rawvideo -ofps $FPS -o $VIDEO_DEVICE
       ;;
     3)
-      ffmpeg -f x11grab -vc rawvideo -s 1280x960 -r $FPS -an -i :0.0$GRAB_OFFSET -f rawvideo -pix_fmt rgb24 - | mencoder - -demuxer rawvideo -rawvideo fps=$FPS:w=1280:h=960:format=rgb24 -nosound -ovc raw -vf hue=0:0,scale=320:240,format=bgr24 -of rawvideo -ofps $FPS -o $VIDEO_DEVICE
+      ffmpeg -f x11grab -vc rawvideo -s ${IN_WIDTH}x${IN_HEIGHT} -r $FPS -an -i :0.0$GRAB_OFFSET -f rawvideo -pix_fmt rgb24 - | mencoder - -demuxer rawvideo -rawvideo fps=$FPS:w=$IN_WIDTH:h=$IN_HEIGHT:format=rgb24 -nosound -ovc raw -vf hue=0:0,scale=320:240,format=bgr24 -of rawvideo -ofps $FPS -o $VIDEO_DEVICE
       ;;
     l|L)
       lsmod | grep avld &> /dev/null
