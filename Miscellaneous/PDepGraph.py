7c5317201d3b9ba05c54d6a170e21bc86cce71af
livibetter <livibetter@localhost>
2009-05-06 17:38:51 +0000

    Rewrote PDepGraph.py

--- Miscellaneous/PDepGraph.py
+++ Miscellaneous/PDepGraph.py
@@ -10,175 +10,326 @@
 # Dependencies:
 #  1. gentoolkit
 #  2. Graphviz - Optional, but need it to generate real graph.
-#
-# Usages:
-#  Generate whole world
-#   ./PDepGraph.py
-#  Pipe result to Graphviz
-#   ./PDepGraph.py | neato -T svg -o world.svg
-#  Generate output of whole world to file world.DOT, then use Graphviz
-#   ./PDepGraph.py -o world.DOT
-#   neato -T svg -o world.svg world.DOT
-#  Highlight package portage
-#   ./PDepGraph.py -l portage
-#  Generate dependency graph of package python
-#   ./PDepGraph.py -p python
 
 
 from optparse import OptionParser
 from sets import Set
 import sys
 
+
+DEBUG = False
+QUIET = False
+DEP_TYPE_COLOR = {'ct': 'red', 'pm': 'green', 'rt': 'blue'}
+
+
+def p(msg, lr=True):
+
+  if QUIET:
+    return
+
+  if lr:
+    print >> sys.stderr, msg
+  else:
+    print >> sys.stderr, msg,
+
+def p_dbg(msg):
+
+  if QUIET or not DEBUG:
+    return
+  p('DEBUG: %s' % msg)
+
+
+def p_err(msg):
+  
+  if QUIET:
+    return
+  p('ERROR: %s' % msg)
+
+
 sys.path += ['/usr/lib/gentoolkit/pym']
 
+
 try:
-  import gentoolkit.helpers
+  from gentoolkit.helpers import find_packages as find_pkgs
+  from gentoolkit.helpers import split_package_name
 except ImportError:
-  print >> sys.stderr, 'You do not seem to have package gentoolkit. Run `emerge gentoolkit` first.'
+  p('You do not seem to have package gentoolkit. Run `emerge gentoolkit` first.')
   sys.exit(1)
 
 
-def get_packages():
+def split_pkg_name(pkg_name):
 
-  packages = {}
+  names = split_package_name(pkg_name)
+  if names[3] == 'r0':
+    names[3] = ''
+  return names
+
+
+def get_ver_rev(pkg_name):
+
+  names = split_package_name(pkg_name)
+  if names[3] == 'r0':
+    return names[2]
+  else:
+    return '-'.join(names[2:])
+
+
+def conflict_pkgs(pkgs):
+
+  cat = pkgs[0].get_category()
+  conflicted = filter(lambda pkg: cat != pkg.get_category(), pkgs[1:])
+  if conflicted:
+    return True
+  return False
+
+
+def find_pkg(pkg_name):
+
+  # Check if slot is in search key
+  if ':' not in pkg_name:
+    found_pkgs = find_pkgs(pkg_name)
+  else:
+    new_pkg_name, slot = pkg_name.split(':')
+    tmp_found_pkgs = find_pkgs(new_pkg_name)
+    # Only keep those in same slot
+    found_pkgs = []
+    for pkg in tmp_found_pkgs:
+      if pkg.get_env_var('SLOT') == slot:
+        found_pkgs += [pkg]
+
+  if len(found_pkgs) == 0:
+    return
+  elif len(found_pkgs) > 1:
+    # Checking if conflict
+    if conflict_pkgs(found_pkgs):
+      p_err('Conflicting package name "%s":' % pkg_name)
+      for pkg in found_pkgs:
+        p_err(' %s' % pkg.get_cpv())
+      return
+    else:
+      # Choose latest version/revision
+      pkg = found_pkgs[0]
+      for pkg_next in found_pkgs[1:]:
+        if pkg.compare_version(pkg_next) == -1:
+          pkg = pkg_next
+  else:
+    pkg = found_pkgs[0]
+  del found_pkgs
   
-  print >> sys.stderr, 'Retrieving packages information...',
-  for package in gentoolkit.helpers.find_all_installed_packages():
-    uses = package.get_use_flags().replace('\n', '').split(' ')
-    uses += ['']
+  return pkg
 
-    # Find out which dependency is enabled by USE flag at the time of emerging.
-    deps = []
-    for dep in package.get_runtime_deps():
+def process_pkg(pkg_name, pkgs, pkg_pool, options):
+
+  pkg = find_pkg(pkg_name)
+  if not pkg:
+    return
+
+  if options.only_installed and not pkg.is_installed():
+    return
+  if pkg.get_name() == 'portage':
+    p_dbg(repr(pkg.get_runtime_deps()))
+  cpv = pkg.get_cpv()
+  if cpv in pkgs:
+    p_dbg('Already processed package %s' % cpv)
+    return
+  else:
+    p_dbg('Processing package %s' % cpv)
+
+  p_pkg = {'cpv': pkg.get_cpv(), 'deps': [], 'installed': pkg.is_installed(), 'found': True}
+
+  # Process dependencies
+  for check_dep_type in options.check_dep_types:
+    if check_dep_type == 'ct':
+      deps = pkg.get_compiletime_deps()
+    elif check_dep_type == 'pm':
+      deps = pkg.get_postmerge_deps()
+    elif check_dep_type == 'rt':
+      deps = pkg.get_runtime_deps()
+
+    for dep in deps:
       # dep = (comparator, [use flags], cpv)
-      for sub_dep in dep[1]:
-        if sub_dep not in uses:
+      # Find dependency package with criterion, which is the comparator
+      if not (options.show_uses or options.only_used_uses) and len(dep[1]) > 0:
+        continue
+      
+      dep_pkg = find_pkg(dep[0] + dep[2])
+      # Check if this dependency is enabled by USE flag
+      used = False
+      uses = pkg.get_use_flags().replace('\n', '').split(' ')
+      for dep_use in dep[1]:
+        neg = False
+        if dep_use[0] == '!':
+          dep_use = dep_use[1:]
+          neg = True
+        if not (dep_use in uses) ^ neg:
           break
       else:
-        dep_name = dep[2].split('/')[1]
-        # Remove version number if any
-        if dep[0]:
-          deps += [dep_name.rsplit('-', 1)[0]]
-        else:
-          deps += [dep_name]
-
-    packages[package.get_name()] = {'revdeps': [], 'deps': deps}
-
-  # Remove deps are not installed
-  for name, value in packages.iteritems():
-    new_deps = value['deps'][:]
-    for dep in value['deps']:
-      if dep not in packages:
-        new_deps.remove(dep)
-    if new_deps != value['deps']:
-      packages[name]['deps'] = new_deps
-  print >> sys.stderr, 'done.'
-
-  print >> sys.stderr, 'Calculating reverse dependencies...',
-  for name, value in packages.iteritems():
-    for dep in value['deps']:
-      packages[dep]['revdeps'] += [name]
-
-  print >> sys.stderr, 'done.'
-  
-  print >> sys.stderr, 'Got %d packages.\n' % len(packages)
-  return packages
-
-
-def generate_DOT(packages, out, hl_pkg_name=''):
-
-  max_revdeps = 0
-  for name, value in packages.iteritems():
-    if len(value['revdeps']) > max_revdeps:
-      max_revdeps = len(value['revdeps'])
-  max_revdeps = float(max_revdeps)
-
-  print >> out, '''digraph Portage {
-overlap=portho
-splines=true
-node [shape=plaintext, fontname=Terminus, fontsize=8.0, fontcolor=red];
-'''
-  for name, value in packages.iteritems():
-    if max_revdeps > 0.0:
-      r_revdeps = len(value['revdeps']) / max_revdeps
-    else:
-      r_revdeps = 0
-
-    if hl_pkg_name and hl_pkg_name == name:
-      print >> out, '"%s" [shape=ellipse, fontname=Terminus, fontsize=8.0, fontcolor=white, color=red, style=filled];' % name
-    else:
-      print >> out, '"%s" [shape=plaintext, fontname=Terminus, fontsize=8.0, fontcolor="#0000%2x"];' % (name, r_revdeps * 255)
-  print >> out
-
-  color_names = ['black', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'crimson', 'darkgreen', 'deeppink', 'dimgray', 'dodgerblue', 'firebrick', 'forestgreen', 'gold', 'green', 'hotpink', 'indigo', 'limegreen', 'magenta', 'navy', 'orange', 'red', 'yellow']
-  #color_names = ['lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrod', 'lightgoldenrodyellow', 'lightgray', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslateblue', 'lightslategray', 'lightyellow']
-  i = 0
-  for name, value in packages.iteritems():
-    for revdep in value['revdeps']:
-      print >> out, '"%s" -> "%s" [arrowhead=vee, arrowsize=0.5, color=%s];' % (name, revdep, color_names[i % len(color_names)])
-    i += 1
-  print >> out, '}'
-
-
-def generate_dep_pkgs(packages, name):
-
-  pkg_names = [name]
-
-  def walker(packages, name, walked_pkgs):
-
-    for pkg in packages[name]['deps']:
-      if pkg in walked_pkgs:
+        used = True
+      if options.only_used_uses and not used:
+        p_dbg('%s: dep %s dropped, USEs not satifified' % (cpv, dep[2]))
         continue
+
+      if dep_pkg:
+        installed = dep_pkg.is_installed()
+        if options.only_installed and not installed:
+          if dep_pkg_cpv not in pkgs:
+            pkgs[dep_pkg_cpv] = {'cpv': dep_pkg_cpv, 'deps': [], 'installed': installed, 'found': True}
+          continue
+        dep_pkg_cpv = dep_pkg.get_cpv()
+        if dep_pkg.get_name() not in options.stop_deps and \
+            dep_pkg.get_category() not in options.stop_cats:
+          pkg_pool += [dep_pkg_cpv]
+        elif dep_pkg_cpv not in pkgs:
+          pkgs[dep_pkg_cpv] = {'cpv': dep_pkg_cpv, 'deps': [], 'installed': installed, 'found': True}
+        p_pkg['deps'] += [(dep, dep_pkg_cpv, check_dep_type, used)]
+        #p_pkg['deps'] += [(dep, dep_pkg_cpv, check_dep_type, True, used, installed)]
+        p_dbg('%s: added dep %s' % (cpv, dep_pkg_cpv))
       else:
-        walked_pkgs += [pkg]
-        walker(packages, pkg, walked_pkgs)
+        if dep[2] not in pkgs:
+          pkgs[dep[2]] = {'cpv': dep[2], 'deps': [], 'installed': False, 'found': False}
+        p_pkg['deps'] += [(dep, dep[2], check_dep_type, used)]
+        #p_pkg['deps'] += [(dep, dep[2], check_dep_type, False, used, False)]
+        p_err('%s: failed to find dep %s' % (cpv, dep[0] + dep[2]))
 
-  walker(packages, name, pkg_names)
+  pkgs[cpv] = p_pkg
 
-  # Clean up unwanted trails
-  set_pkg_names = Set(pkg_names)
-  pkgs = {}
-  for pkg_name in pkg_names:
-    pkg = packages[pkg_name]
-    pkg['deps'] = list(Set(pkg['deps']) & set_pkg_names)
-    pkg['revdeps'] = list(Set(pkg['revdeps']) & set_pkg_names)
-    pkgs[pkg_name] = pkg
 
-  print >> sys.stderr, 'Found %d packages for package %s.\n' % (len(pkgs), name)
-  return pkgs
+def generate_DOT(pkgs, out):
+
+  out('''digraph Portage {
+rankdir=LR
+node [shape=plaintext, fontname=Terminus, fontsize=8.0, fontcolor=black];
+''')
+  # Print all packages
+  for pkg_name in pkgs:
+    pkg = pkgs[pkg_name]
+    names = split_pkg_name(pkg_name)
+    pkg_attrs = {'fontname': 'Terminus', 'fontsize': '8.0', 'fontcolor': 'black', 'label': r'"%s\n%s"' % ('%s/%s' % (names[0], names[1]), get_ver_rev(pkg_name))}
+    if pkg['installed']:
+      pkg_attrs['shape'] = 'box'
+    else:
+      pkg_attrs['shape'] = 'plaintext'
+    if not pkg['found']:
+      pkg_attrs['fillcolor'] = 'red'
+      pkg_attrs['fontcolor'] = 'white'
+      pkg_attrs['style'] = 'filled'
+    if names[1] in options.stop_deps or names[0] in options.stop_cats:
+      pkg_attrs['fontcolor'] = 'white'
+      pkg_attrs['fillcolor'] = 'blue'
+      pkg_attrs['style'] = 'filled'
+    out(r'"%s" [%s];' % (pkg_name, ','.join(['%s=%s' % (k, v) for k, v in pkg_attrs.items()])))
+
+  # Print all deps
+  for pkg_name in pkgs:
+    pkg = pkgs[pkg_name]
+    for dep in pkg['deps']:
+      dep_info, dep_pkg_cpv, dep_type, used = dep
+      dep_attrs = {'fontname': 'Terminus', 'fontsize': '8.0', 'fontcolor': 'black', 'color': DEP_TYPE_COLOR[dep_type]}
+      if dep_info[0]:
+        # Has comparator
+        dep_attrs['label'] = '"%s"' % (dep_info[0] + get_ver_rev(dep_info[2]))
+      elif ':' in dep_info[2]:
+        # A SLOT dependency
+        dep_attrs['label'] = '":%s"' % dep_info[2].split(':')[1]
+      if dep_info[1]:
+        dep_attrs['style'] = 'dashed'
+        # TODO put USE flags into dep_attrs['label']
+      if used:
+        # This dependency is enabled by USE flag
+        dep_attrs['penwidth'] = '2'
+
+      out('"%s" -> "%s" [%s]' % (pkg_name, dep_pkg_cpv, ','.join(['%s=%s' % (k, v) for k, v in dep_attrs.items()])))
+  out('}')
 
 
 if __name__ == '__main__':
-  parser = OptionParser()
-  parser.add_option('-p', '--package', dest='package',
-                    help='find dependencies of package, also set highlight.')
-  parser.add_option('-l', '--highlight', dest='highlight',
-                    help='highlight package.')
+  parser = OptionParser(usage='%prog [options] package', )
+  parser.add_option('-c', '--ctdeps', dest='ctdeps', action='store_true',
+      default=False, help='Show compile time dependencies')
+  parser.add_option('-p', '--pmdeps', dest='pmdeps', action='store_true',
+      default=False, help='Show post merge dependencies')
+  parser.add_option('-r', '--rtdeps', dest='rtdeps', action='store_true',
+      default=True, help='Show run time dependencies (default)')
+  parser.add_option('-R', '--no-rtdeps', dest='rtdeps', action='store_false',
+      default=True, help='Do not show run time dependencies')
+  parser.add_option('-i', '--only-installed', dest='only_installed', action='store_true',
+      default=False, help='Show only installed package')
+  parser.add_option('-u', '--show-uses', dest='show_uses', action='store_true',
+      default=False, help='Show dependencies with USE flag attached')
+  parser.add_option('-U', '--only-used-uses', dest='only_used_uses', action='store_true',
+      default=False, help='Show dependencies with USE flag attached and acutally used in installing time. This implies -u.')
+  parser.add_option('-C', '--stop-cats', dest='stop_cats',
+      default='virtual', help='Do follow dependencies in these categories (default: %default)')
+  parser.add_option('-D', '--stop-deps', dest='stop_deps',
+      default='openldap', help='Do follow these dependencies (default: %default)')
+  parser.add_option('-H', '--more-help', dest='help', action='store_true',
+      default=False, help='Show more help')
+  parser.add_option('-d', '--debug', dest='debug', action='store_true',
+      default=False, help='Show debug messages')
+  parser.add_option('-q', '--quiet', dest='quiet', action='store_true',
+      default=False, help='Quiet mode')
   parser.add_option('-o', '--out', dest='out',
                     help='write Graphviz DOT to FILE. Using standard output if not specifed.', metavar='FILE')
   (options, args) = parser.parse_args()
 
-  packages = get_packages()
+  if options.help:
+    print '''Legend:
+ Package Shape:
+  None   = The package is not installed
+  Box    = The package is installed
+ Package Color:
+  Red    = The package could not be found
+  Blue   = The package is stopped being followd
+ Arrow Color:
+  Red    = Compile time dependency
+  Green  = Post merge dependency
+  Blue   = Run time dependency
+ Arrow Thickness:
+  Thick  = The dependency is enabled by USE flags at installing time
+  Thin   = The dependency is not enabled by USE flags at installing time
+ Arrow Style:
+  Solid  = Required dependency
+  Dashed = Optional by USE flags
+'''
+    sys.exit(0)
 
-  package = options.package
-  highlight = options.highlight
-  out =  options.out
+  options.stop_cats = options.stop_cats.split(',')
+  options.stop_deps = options.stop_deps.split(',')
+
+  DEBUG = options.debug
+  QUIET = options.quiet
+
+  check_dep_types = []
+  if options.ctdeps:
+    check_dep_types += ['ct']
+  if options.pmdeps:
+    check_dep_types += ['pm']
+  if options.rtdeps:
+    check_dep_types += ['rt']
+  options.check_dep_types = check_dep_types
+
+  out = options.out
   if out:
     f_out = open(out, 'w')
   else:
     f_out = sys.stdout
 
-  if package and package in packages:
-    pkgs = generate_dep_pkgs(packages, package)
-    highlight = package
-  else:
-    pkgs = packages
+  # Store all packages are needed
+  pkgs = {}
+  # A queue of packages waiting to process
+  pkg_pool = [args[0]]
+
+  while pkg_pool:
+    pkg_name = pkg_pool.pop()
+    process_pkg(pkg_name, pkgs, pkg_pool, options) 
+
+  p('Result %d packages' % len(pkgs))
+
+  def print_out(msg):
+    print >> f_out, msg
+
+  generate_DOT(pkgs, print_out)
 
   if out:
-    print >> sys.stderr, 'Writing to %s...' % out,
-  
-  generate_DOT(pkgs, f_out, highlight)
-  
-  if out:
-    print >> sys.stderr, 'done.\n'
-    print >> sys.stderr, 'Generate graph using `neato -T [svg|png|...] -o OUTPUT %s`' % out
+    f_out.close()
+    p('\nGenerate graph using `dot -T [svg|png|...] -o OUTPUT %s`')
