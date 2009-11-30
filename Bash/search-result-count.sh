2ae2009d817cb13fe93a39894dc07c5170b0be10
Yu-Jie Lin <livibetter@gmail.com>
2009-11-30 10:12:13 +0800

    search-result-count.sh: Added Yahoo\! Search and Bing

--- Bash/search-result-count.sh
+++ Bash/search-result-count.sh
@@ -2,13 +2,15 @@
 # search-result-count: Gets the result count from Google AJAX Search API
 #
 # Author: Yu-Jie Lin [http://livibetter.com/]
-# Last Updated: 2009-11-26T12:23:47+0800
+# Last Updated: 2009-11-28T08:04:28+0800
 #
 # This Bash script is put to Public Domain, use at your own risk.
 #
 # Depends:
 #   wget
 #   Google AJAX Search API
+#   Yahoo! Search BOSS
+#   Bing API
 # 	Google Visualization API
 #
 # Usage: search-result-count keyword [render [width [height]]
@@ -17,6 +19,10 @@
 #   search-result-count <keyword>
 #
 #   It will store a record to CSV file at $HOME/.search-result-count-<keyword>
+#
+#   Suggest to run every 3 hours:
+#   #Mins   Hours   Days    Months  Day of the week
+#   0       */3     *       *       *               /path/to/search-result-count <keyword>
 #
 # 2) Run the following to get rendered html:
 #   search-result-count render
@@ -27,6 +33,13 @@
 #
 # Note that this script can not do urlencode for your keyword.
 
+YAHOO_APPID="k7NNT8DV34HFckFK7ZYLD6Ou7MvAW2mqnAi16suw41dvIqOV0OmI6wdf0ot4EyBEDg--"
+BING_APPID="F102804AE27C06275E0B535689E4134DABC92FBA"
+
+# Also can set to "allmaximized" but you may get confused at first.
+# Read http://j.mp/5IOCWG
+SCALE_TYPE="maximized"
+
 get_count() {
 	today="$(date +'%Y,%m,%d')"
 
@@ -35,12 +48,15 @@ get_count() {
 	[[ $? == 0 ]] && exit 1
 
 	# Get the count
-	count=$(wget -q "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=$keyword" -O - | egrep -o '"estimatedResultCount":"[0-9]+"' | cut -d\" -f 4)
-
-	if [[ $count != "" ]]; then
-		[[ ! -f "$filename" ]] && echo "Year,Month,Day,Count" > "$filename"
-		echo "$today,$count" >> "$filename"
-	fi
+	g_count=$(wget -q "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=$keyword" -O - | egrep -o '"estimatedResultCount":"[0-9]+"' | cut -d\" -f 4)
+	[[ $g_count == "" ]] && echo "$(date) Can get Google Search result count for $keyword" >> $HOME/search-result-count.error && exit 1
+	y_count=$(wget -q "http://boss.yahooapis.com/ysearch/web/v1/$keyword?appid=$YAHOO_APPID&format=xml" -O - | egrep -o ' totalhits="[0-9]+"' | cut -d\" -f 2)
+	[[ $y_count == "" ]] && echo "$(date) Can get Yahoo Search result count for $keyword" >> $HOME/search-result-count.error && exit 1
+	b_count=$(wget -q "http://api.bing.net/json.aspx?AppId=$BING_APPID&Version=2.2&Market=en-US&Query=$keyword&Sources=web&Web.Count=1" -O - | egrep -o '"Total":[0-9]+' | cut -d: -f 2)
+	[[ $b_count == "" ]] && echo "$(date) Can get Blog Search result count for $keyword" >> $HOME/search-result-count.error && exit 1
+	
+	[[ ! -f "$filename" ]] && echo "Year,Month,Day,Count" > "$filename"
+	echo "$today,$g_count,$y_count,$b_count" >> "$filename"
 	}
 
 render() {
@@ -48,17 +64,20 @@ render() {
 	echo -n "Generating... "
 	old_IFS=$IFS
 	IFS=","
-	data=$(cat "$filename" | { data="" ; while read y m d count; do
+	data=$(cat "$filename" | { data="" ; while read y m d g_count y_count b_count; do
 		[[ "$y" == "Year" ]] && continue
 		(( m -= 1 ))
-		data="$data          [new Date($y, $m, $d), $count],\n"
+		data="$data          [new Date($y, $m, $d), $g_count, "
+		[[ $y_count != "" ]] && data="$data $y_count, " || data="$data undefined,"
+		[[ $b_count != "" ]] && data="$data $b_count" || data="$data undefined"
+		data="$data],\n"
 		done
 		echo -ne "$data"
 		})
 	IFS=old_IFS
 	echo -n "<html>
   <head>
-    <title>Google Search Result Count for Keyword &#8220$keyword&#8221</title>
+    <title>Search Result Count for Keyword &#8220$keyword&#8221</title>
     <script type='text/javascript' src='http://www.google.com/jsapi'></script>
     <script type='text/javascript'>
       google.load('visualization', '1', {'packages':['annotatedtimeline']});
@@ -66,19 +85,21 @@ render() {
       function drawChart() {
         var data = new google.visualization.DataTable();
         data.addColumn('date', 'Date');
-        data.addColumn('number', '$keyword');
+        data.addColumn('number', 'Google');
+        data.addColumn('number', 'Yahoo');
+        data.addColumn('number', 'Bing');
         data.addRows([
 $data
-          [undefined, undefined]
+          [undefined, undefined, undefined, undefined]
         ]);
 
         var chart = new google.visualization.AnnotatedTimeLine(document.getElementById('chart_div'));
-        chart.draw(data);
+        chart.draw(data, {scaleType: '$SCALE_TYPE'});
       }
     </script>
   </head>
   <body style='text-align: center;'>
-    <h1>Google Search Result Count for Keyword &#8220$keyword&#8221</h1>
+    <h1>Keyword &#8220$keyword&#8221</h1>
     <div id='chart_div' style='width: ${width}px; height: ${height}px; margin: 0 auto;'></div>
   </body>
 </html>" > "$out_filename"
