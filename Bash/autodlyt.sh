f0cca38e3f2a2c8188860cb3a8d22c368a45eeb7
Yu-Jie Lin <livibetter@gmail.com>
2010-11-13 03:10:27 +0800

    Add script for auto-downloading yt new subscription videos and update conkyrc for showing status

---
+++ Bash/autodlyt.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# by Yu-Jie Lin
+# BSD license
+# 2010-11-10T19:13:46+0800
+
+usage () {
+	echo "Usage: $(basename $0) username directory-to-store-everything"
+	exit 1
+}
+
+USERNAME="$1"
+[[ "$USERNAME" == "" ]] && usage
+DESTDIR="$2/$1"
+[[ "$DESTDIR" == "" ]] && usage
+
+DLPROGNAME='youtube-dl'
+DLPROGOPTS='-t'
+
+DLPROG="$(type -p "$DLPROGNAME")"
+if [[ "$DLPROG" == "" ]]; then
+	echo "Could not find $DLPROGNAME."
+	exit 1
+fi
+
+[[ ! -d "$DESTDIR" ]] && mkdir -p "$DESTDIR"
+[[ ! -d "$DESTDIR/errors" ]] && mkdir -p "$DESTDIR/errors"
+[[ ! -d "$DESTDIR/videos" ]] && mkdir -p "$DESTDIR/videos"
+[[ ! -d "$DESTDIR/queue" ]] && mkdir -p "$DESTDIR/queue"
+
+# Downloading feed
+TMPFILE="$(mktemp)"
+dllog="$(wget "http://gdata.youtube.com/feeds/api/users/$USERNAME/newsubscriptionvideos?prettyprint=true&fields=entry(link[@rel='alternate'](@href))" -O "$TMPFILE" 2>&1)"
+if (( $? != 0 )); then
+	echo "$dllog" > "$DESTDIR/errors/dlfeed"
+	echo "$dllog" 1>&2
+	rm "$TMPFILE"
+	exit 1
+fi
+
+videos="$(grep link "$TMPFILE" | sed 's/.*v=\(.*\)&.*/\1/')"
+rm "$TMPFILE"
+
+lastvideo="$(cat "$DESTDIR"/last 2>/dev/null)"
+
+for v in $videos; do
+	[[ "$lastvideo" == "$v" ]] && break
+	[[ ! -f "$DESTDIR/queue/$v" ]] && echo "Queuing $v" && echo "http://www.youtube.com/watch?v=$v" > "$DESTDIR/queue/$v"
+done
+
+newlastvideo="$(head -1 <<< "$videos")"
+[[ "$newlastvideo" != "$lastvideo" ]] && echo "Updating $DESTDIR/last" && echo "$newlastvideo" > "$DESTDIR"/last
+
+videos="$(ls "$DESTDIR"/queue/* 2>/dev/null)"
+[[ "$videos" == "" ]] && exit
+
+cd "$DESTDIR/videos"
+for v in $videos; do
+	# Get the URL of video
+	vid="$(basename "$v")"
+	url="http://www.youtube.com/watch?v=$vid"
+
+	echo "Downloading $url using $DLPROG..."
+	dllog="$("$DLPROG" $DLPROGOPTS "$url")"
+	if (( $? != 0 )); then
+		# Error on downloading, just write the output to errors directory
+		echo "$dllog" > "$DESTDIR/errors/$vid"
+		echo "$dllog" 1>&2
+	fi
+	rm "$v"
+done
