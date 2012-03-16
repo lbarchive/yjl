dc0bb46206d8e97bb5c8d7415a7e59bcfc103ca8
Yu-Jie Lin <livibetter@gmail.com>
2012-03-16 14:51:59 +0800

    Remove yjl-im
    It has been moved to https://bitbucket.org/livibetter/yjl.im

--- GoogleAppEngine/yjl-im/src/css/gen-yjlv.css.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cat ../font/stylesheet.css highlight-zenburn.css yjlv-base.css | sed "s_url('_url('../font/_g" | yuicompressor.sh --type css -o yjlv.css
--- GoogleAppEngine/yjl-im/src/js/gen-yjlv.js.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-closure.sh --js=yjlv-base.js --js_output_file=/tmp/yjlv-base.min.js
-cat jquery.min.js /tmp/yjlv-base.min.js > yjlv.js
